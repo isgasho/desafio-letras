799b37115eee7a3caf3208b2dc62989b15d2600e
Vinícius Rodrigues Miguel <lemao.vrm07@hotmail.com>
2021-03-17 00:21:31 -0300

    repl: Show unused letters, improve output formatting

--- src/repl.rs
+++ src/repl.rs
@@ -1,21 +1,53 @@
-use std::io::{self, BufRead};
+use std::{collections::HashMap, io::{self, BufRead, Write}};
 
 use colored::Colorize;
 
 use crate::{Word, get_solution};
 
+fn print_letters_left(occurrences: HashMap<char, u32>) {
+    
+    let mut letters = Vec::with_capacity(5);
+
+    for (letter, count) in occurrences {
+        match count {
+            0 => continue,
+            1 => letters.push(letter),
+            count => {
+                for _ in 0..count {
+                    letters.push(letter);
+                }
+            }
+        }
+    }
+
+    let len = letters.len();
+
+    letters
+        .into_iter()
+        .enumerate()
+        .for_each(|(idx, letter)| 
+            if idx == len-1 {
+                print!("{}\n", letter);
+            } else {
+                print!("{}, ", letter)
+            }
+        );
+}
+
 pub fn start_loop() {
     let mut line = String::new();
     loop {
         eprint!("Digite as letras disponíveis nesta jogada: ");
+        
         if io::stdin().read_line(&mut line).is_err() {
             eprintln!("{}: houve um problema na leitura de palavra.", "erro".red());
             continue;
         }
+
         let word = Word::new(line.trim());
         line.clear();
 
-        let (solution, words_left) = get_solution(word);
+        let (solution, letters_left) = get_solution(word);
 
         match solution.len() {
             0 => println!("Nenhuma palavra encontrada"),
@@ -24,21 +56,19 @@ pub fn start_loop() {
                 println!("{}, palavra de {} pontos.", word.word, word.score);
             },
             _ => {
-                solution.iter().for_each(|word|
-                    print!("{} ", word.word)
-                );
+                for word in solution.iter() {
+                    print!("{}, ", word.word)
+                }
                 let sum: u32 = solution.iter().map(|word| word.score).sum();
+                print!("total de {} pontos.\n", sum);
             }
         }
 
-        // match get_best_move(&word) {
-        //     Some(best_move) => {
-        //         println!("Best move: {}", best_move);
-        //     },
-        //     None => {
-        //         println!("Nenhuma palavra encontrada.");
-        //         // TODO: print letters remaining
-        //     }
-        // }
+        if !letters_left.is_empty() {
+            print!("Sobraram: ");
+            print_letters_left(letters_left);
+        }
+
+        io::stdout().flush().unwrap();
     }
 }
